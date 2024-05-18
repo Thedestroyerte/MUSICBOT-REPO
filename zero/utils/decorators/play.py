d55f9ea35c469c2d41e471d6a4eac2a1c84ce63a
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-18 15:23:21 +0530

    Add files via upload

---
+++ zero/utils/decorators/play.py
@@ -0,0 +1,187 @@
+import asyncio
+from Zero.misc import SUDOERS
+from Zero import YouTube, app
+from Zero.Nxz import get_string
+from pyrogram.enums import ChatMemberStatus
+from Zero.utils.exceptions import AssistantErr
+from Zero.utils.inline.playlist import botplaylist_markup
+from Zero.utils.database.memorydatabase import is_maintenance
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from Zero.config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
+from pyrogram.errors import ChatAdminRequired, UserAlreadyParticipant, UserNotParticipant
+from Zero.utils.database import get_cmode, get_lang, get_playmode, get_playtype, is_active_chat, is_commanddelete_on, is_served_private_chat
+from Zero.utils.database import add_active_chat, add_active_video_chat, get_assistant, get_audio_bitrate, get_lang, get_loop, get_video_bitrate, group_assistant, is_autoend, music_on, mute_off, remove_active_chat, remove_active_video_chat, set_loop
+
+def PlayWrapper(command):
+    async def wrapper(client, message):
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    "Bot is under maintenance. Please wait for some time..."
+                )
+        if PRIVATE_BOT_MODE == str(True):
+            if not await is_served_private_chat(message.chat.id):
+                await message.reply_text(
+                    "**Private Music Bot**\n\nOnly for authorized chats from the owner. Ask my owner to allow your chat first."
+                )
+                return await app.leave_chat(message.chat.id)
+        if await is_commanddelete_on(message.chat.id):
+            try:
+                await message.delete()
+            except:
+                pass
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+        audio_telegram = (
+            (
+                message.reply_to_message.audio
+                or message.reply_to_message.voice
+            )
+            if message.reply_to_message
+            else None
+        )
+        video_telegram = (
+            (
+                message.reply_to_message.video
+                or message.reply_to_message.document
+            )
+            if message.reply_to_message
+            else None
+        )
+        url = await YouTube.url(message)
+        if (
+            audio_telegram is None
+            and video_telegram is None
+            and url is None
+        ):
+            if len(message.command) < 2:
+                if "stream" in message.command:
+                    return await message.reply_text(_["str_1"])
+                buttons = botplaylist_markup(_)
+                return await message.reply_photo(
+                    photo=PLAYLIST_IMG_URL,
+                    caption=_["playlist_1"],
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
+        if message.sender_chat:
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="How to Fix this? ",
+                            callback_data="AnonymousAdmin",
+                        ),
+                    ]
+                ]
+            )
+            return await message.reply_text(
+                _["general_4"], reply_markup=upl
+            )
+        if message.command[0][0] == "c":
+            chat_id = await get_cmode(message.chat.id)
+            if chat_id is None:
+                return await message.reply_text(_["setting_12"])
+            try:
+                chat = await app.get_chat(chat_id)
+            except:
+                return await message.reply_text(_["cplay_4"])
+            channel = chat.title
+        else:
+            chat_id = message.chat.id
+            channel = None
+        playmode = await get_playmode(message.chat.id)
+        playty = await get_playtype(message.chat.id)
+        if playty != "Everyone":
+            if message.from_user.id not in SUDOERS:
+                admins = adminlist.get(message.chat.id)
+                if not admins:
+                    return await message.reply_text(_["admin_18"])
+                else:
+                    if message.from_user.id not in admins:
+                        return await message.reply_text(_["play_4"])
+        if message.command[0][0] == "v":
+            video = True
+        else:
+            if "-v" in message.text:
+                video = True
+            else:
+                video = True if message.command[0][1] == "v" else None
+        if message.command[0][-1] == "e":
+            if not await is_active_chat(chat_id):
+                return await message.reply_text(_["play_18"])
+            fplay = True
+        else:
+            fplay = None
+        userbot = await get_assistant(chat_id)
+        try:
+                try:
+                    get = await app.get_chat_member(chat_id, userbot.id)
+                except ChatAdminRequired:
+                    return await message.reply_text(_["call_1"])
+                if (
+                    get.status == ChatMemberStatus.BANNED
+                    or get.status == ChatMemberStatus.LEFT
+                ):
+                    return await message.reply_text(
+                        _["call_2"].format(userbot.username, userbot.id)
+                        )
+        except UserNotParticipant:
+            chat = await app.get_chat(chat_id)
+            if chat.username:
+                try:
+                    await userbot.join_chat(chat.username)
+                except UserAlreadyParticipant:
+                    pass
+                except Exception as e:
+                    raise AssistantErr(_["call_3"].format(e))
+            else:
+                try:
+                    try:
+                        try:
+                            invitelink = chat.invite_link
+                            if invitelink is None:
+                                invitelink = (
+                                    await app.export_chat_invite_link(
+                                        chat_id
+                                    )
+                                )
+                        except:
+                            invitelink = (
+                                await app.export_chat_invite_link(
+                                    chat_id
+                                )
+                            )
+                    except ChatAdminRequired:
+                        raise AssistantErr(_["call_4"])
+                    except Exception as e:
+                        raise AssistantErr(e)
+                    m = await app.send_message(
+                        chat_id, _["call_5"]
+                    )
+                    if invitelink.startswith("https://t.me/+"):
+                        invitelink = invitelink.replace(
+                            "https://t.me/+", "https://t.me/joinchat/"
+                        )
+                    await asyncio.sleep(0.0001)
+                    await userbot.join_chat(invitelink)
+                    await asyncio.sleep(0.00001)
+                    await m.edit(_["call_6"].format(userbot.name))
+                except UserAlreadyParticipant:
+                    pass
+                except Exception as e:
+                    raise AssistantErr(_["call_3"].format(e))
+                    await message.reply_text(_["call_3"])
+                  
+        return await command(
+            client,
+            message,
+            _,
+            chat_id,
+            video,
+            channel,
+            playmode,
+            url,
+            fplay,
+        )
+
+    return wrapper
