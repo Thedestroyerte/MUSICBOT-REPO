f7eabe346b4d12e8be5c6e19a6e1a44e3fa37c90
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-18 15:24:48 +0530

    Add files via upload

---
+++ zero/utils/stream/autoclear.py
@@ -0,0 +1,26 @@
+import os
+
+from Zero.config import autoclean
+
+async def auto_clean(popped):
+    try:
+        rem = popped["file"]
+        autoclean_copy = autoclean.copy()
+        for item in autoclean_copy:
+            if item == rem:
+                autoclean.remove(item)
+
+        count = autoclean.count(rem)
+        if count == 0:
+            if (
+                "vid_" not in rem
+                and "live_" not in rem
+                and "index_" not in rem
+            ):
+                try:
+                    if os.path.exists(rem):
+                        os.remove(rem)
+                except:
+                    pass
+    except:
+        pass
