42176ab4ef2d62dac9c096a0a179903952244857
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-18 14:45:02 +0530

    Add files via upload

---
+++ plugins/bot/help.py
@@ -0,0 +1,184 @@
+# Powered By @ZeroNxz & @Professor7x
+# Join @ZeronxzTech For More Updates
+# Join @Zero For Hacks
+# Join Our Chats @ZeroChats & @Professor7x
+
+
+
+from typing import Union
+
+from pyrogram import filters, types
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from Zero import config
+from Zero.config import BANNED_USERS
+from Zero.nxz import get_command, get_string, helpers
+from Zero import app
+from Zero.misc import SUDOERS
+from Zero.utils import help_pannel
+from Zero.utils.database import get_lang, is_commanddelete_on
+from Zero.utils.decorators.language import (LanguageStart,
+                                                  languageCB)
+from Zero.utils.inline.help import (help_back_markup,
+                                          private_help_panel)
+
+### Command
+HELP_COMMAND = get_command("HELP_COMMAND")
+
+
+@app.on_message(
+    filters.command(HELP_COMMAND)
+    & filters.private
+    & ~BANNED_USERS
+)
+@app.on_callback_query(
+    filters.regex("settings_back_helper") & ~BANNED_USERS
+)
+async def helper_private(
+    client: app, update: Union[types.Message, types.CallbackQuery]
+):
+    is_callback = isinstance(update, types.CallbackQuery)
+    if is_callback:
+        try:
+            await update.answer()
+        except:
+            pass
+        chat_id = update.message.chat.id
+        language = await get_lang(chat_id)
+        _ = get_string(language)
+        keyboard = help_pannel(_, True)
+        if update.message.photo:
+            await update.edit_message_text(
+                _["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
+            )
+        else:
+            await update.edit_message_text(
+                _["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
+            )
+    else:
+        chat_id = update.chat.id
+        if await is_commanddelete_on(update.chat.id):
+            try:
+                await update.delete()
+            except:
+                pass
+        language = await get_lang(chat_id)
+        _ = get_string(language)
+        keyboard = help_pannel(_)
+        await update.reply_photo(
+            photo=config.START_IMG_URL,
+            caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
+
+
+@app.on_message(
+    filters.command(HELP_COMMAND)
+    & filters.group
+    & ~BANNED_USERS
+)
+@LanguageStart
+async def help_com_group(client, message: Message, _):
+    keyboard = private_help_panel(_)
+    await message.reply_photo(
+        photo=config.START_IMG_URL,
+        caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+
+@app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
+@languageCB
+async def helper_cb(client, CallbackQuery, _):
+    callback_data = CallbackQuery.data.strip()
+    cb = callback_data.split(None, 1)[1]
+    keyboard = help_back_markup(_)
+    if cb == "hb9":
+        if CallbackQuery.from_user.id not in SUDOERS:
+            return await CallbackQuery.answer(
+                "𝐓𝐡𝐢𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 𝐈𝐬 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐒𝐮𝐝𝐨 𝐔𝐬𝐞𝐫'𝐬.", show_alert=True
+            )
+        else:
+            await CallbackQuery.edit_message_text(
+                helpers.HELP_9, reply_markup=keyboard
+            )
+            return await CallbackQuery.answer()
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
+    if cb == "hb1":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_1, reply_markup=keyboard
+        )
+    elif cb == "hb2":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_2, reply_markup=keyboard
+        )
+    elif cb == "hb3":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_3, reply_markup=keyboard
+        )
+    elif cb == "hb4":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_4, reply_markup=keyboard
+        )
+    elif cb == "hb5":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_5, reply_markup=keyboard
+        )
+    elif cb == "hb6":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_6, reply_markup=keyboard
+        )
+    elif cb == "hb7":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_7, reply_markup=keyboard
+        )
+    elif cb == "hb8":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_8, reply_markup=keyboard
+        )
+    elif cb == "hb14":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_14, reply_markup=keyboard
+        )
+    elif cb == "hb10":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_10, reply_markup=keyboard
+        )
+    elif cb == "hb11":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_11, reply_markup=keyboard
+        )
+    elif cb == "hb5":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_5, reply_markup=keyboard
+        )
+    elif cb == "hb13":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_13, reply_markup=keyboard
+        )
+    elif cb == "hb15":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_15, reply_markup=keyboard
+        )
+    elif cb == "hb16":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_16, reply_markup=keyboard
+        )
+    elif cb == "hb17":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_17, reply_markup=keyboard
+        )
+    elif cb == "hb18":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_18, reply_markup=keyboard
+        )
+    elif cb == "hb12":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_12, reply_markup=keyboard
+        )
+
+
+# Powered By @ZeroNxz & @Professor7x
+# Join @ZeronxzTech For More Updates
+# Join @Zero For Hacks
+# Join Our Chats @ZeroChats & @Professor7x
